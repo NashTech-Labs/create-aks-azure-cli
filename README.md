d96e1d971b5a5b382c19a8cc6ca24d5e908cc461
shivamgupta-sg <shivam.gupta@knoldus.com>
2023-01-08 02:59:10 +0530

    added shell script and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# create-aks-azure-cli
-Shell Script to create AKS CLuster
+# What is a Azure Kubernetes Service (AKS)?
+
+- **Azure Kubernetes Service** is a managed container orchestration service based on the open source Kubernetes system. 
+- AKS can handle critical functionality such as deploying, scaling and managing Docker containers and container-based applications.
+- AKS became generally available in June 2018
+- There can be multiple resource group in a single subscription
+
+![Azure Kubernetes Service](https://miro.medium.com/max/1890/1*l6oSrwfJhPXdfA92KYtfzA.png)
+
+You can get the more information [here](https://learn.microsoft.com/en-us/azure/cloud-adoption-framework/govern/resource-consistency/resource-access-management)
+
+# What is Azure CLI?
+
+- The Azure Command-Line Interface (CLI) is a cross-platform command-line tool to connect to Azure and execute administrative commands on Azure resources.
+- It allows the execution of commands through a terminal using interactive command-line prompts or a script.
+
+You can get the more information [here](https://learn.microsoft.com/en-us/cli/azure/)
+
+# Pre-requsties
+
+- Setup Azure CLI, check for the installation of Azure CLI [here](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli)
+
+# How to run the script
+
+- Run the script using command `./create-aks.sh`
+- Enter the resource group name, location of resource group, vnet name, vnet address prefix (optional), subnet name, address prefix for subnet, aks name, aks network plugin when asked
---
+++ create-aks.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+set -o errexit
+set -o xtrace  # used for debugging
+
+# az provider list --query "[?contains(namespace,'Microsoft.ContainerInstance')]" -o table
+
+##################################################
+
+# Variables for Azure Resource Group
+echo "\nEnter Resource Group Name: "
+read rg_name
+
+echo "\nEnter Resource Group Location: "
+read rg_location
+
+# Create Azure Resource Group
+echo "Creating an Azure Resource Group"
+create_rg_status=$(az group create \
+    --name $rg_name \
+    --location $rg_location)
+
+# create_rg_status=$(echo "creating rg")
+
+# check status code for resource group creation
+create_rg_status_code=$?
+
+[ $create_rg_status_code -eq 0 ] && echo "\nResource Group Created" || echo "\nError creating Resource Group \n $create_rg_status"
+
+##################################################
+
+# Variables for Azure Vnet
+echo "\nEnter VNet Name: "
+read vnet_name
+
+echo "\nEnter VNet Address Prefix: "
+read vnet_address_prefix
+
+# Create Azure Vnet
+echo "Creating an Azure VNet"
+
+if [ -z "$vnet_address_prefix" ]
+then
+    # create_vnet_status=$(echo "creating vnet without address prefix")
+    create_vnet_status=$(az network vnet create \
+        --resource-group $rg_name \
+        --name $vnet_name)
+else
+    # create_vnet_status=$(echo "creating vnet with address prefix")
+    create_vnet_status=$(az network vnet create \
+        --resource-group $rg_name \
+        --name $vnet_name \
+        --address-prefixes $vnet_address_prefix)
+fi
+
+# check status code for vnet creation
+create_vnet_status_code=$?
+
+[ $create_vnet_status_code -eq 0 ] && echo "\nVNet Created" || echo "\nError creating VNet \n $create_vnet_status"
+
+# az network vnet create \
+#     --resource-group $rg_name \
+#     --name $vnet_name \
+#     --address-prefixes 10.0.0.0/8 \
+#     # --subnet-name myAKSSubnet \
+#     # --subnet-prefix 10.240.0.0/16
+
+##################################################
+
+# Variables for Azure Subnet
+echo "\nEnter Subnet Name: "
+read subnet_name
+
+echo "\nEnter Subnet Address Prefix: "
+read subnet_address_prefix
+
+# Create Azure VNet Subnet
+echo "Creating an Azure VNet Subnet"
+
+# create_subnet_status=$(echo "subnet creating")
+create_subnet_status=$(az network vnet subnet create \
+    --resource-group $rg_name \
+    --vnet-name $vnet_name \
+    --name $subnet_name \
+    --address-prefixes $subnet_address_prefix)
+
+# check status code for subnet creation
+create_subnet_status_code=$?
+
+[ $create_subnet_status_code -eq 0 ] && echo "\nSubnet Created" || echo "\nError creating Subnet \n $create_subnet_status"
+
+
+# Get Subnet ID
+subnet_id=$(az network vnet subnet show \
+    --resource-group $rg_name \
+    --vnet-name $vnet_name \
+    --name $subnet_name \
+    --query id \
+    -o tsv)
+
+##################################################
+
+# Variables for AKS
+echo "\nEnter AKS Name: "
+read aks_name
+
+echo "\nEnter Node Count: "
+read aks_node_count
+
+echo "\nEnter Netwrok Plugin for AKS Cluster: "
+read aks_network_plugin
+
+# Create AKS
+echo "Creating AKS Cluster"
+create_aks_status=$(az aks create \
+    --resource-group $rg_name \
+    --name $aks_name \
+    --node-count $aks_node_count \
+    --network-plugin $aks_network_plugin \
+    --vnet-subnet-id $subnet_id \
+    --generate-ssh-keys )
+
+# check status code for aks creation
+create_aks_status_code=$?
+
+[ $create_aks_status_code -eq 0 ] && echo "\nAKS Created" || echo "\nError creating AKS \n $create_aks_status"
